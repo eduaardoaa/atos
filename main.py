116e4a8f297fe525cad8c857d46ba316ab60e1cb
Eduardo Andrade <144062419+eduaardoaa@users.noreply.github.com>
2025-03-26 22:33:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ def conexaobanco():
     try:
         conn = mysql.connector.connect(
             host=os.getenv("DB_HOST"),
-            port=3306,
             user=os.getenv("DB_USER"),
             password=os.getenv("DB_PASSWORD"),
             database=os.getenv("DB_NAME")
