85e24f255cbe3557fcbc7f8299b39a988cf4337e
Eduardo Andrade <144062419+eduaardoaa@users.noreply.github.com>
2025-05-21 23:18:33 -0300

    Update adm.py

--- adm.py
+++ adm.py
@@ -18,11 +18,11 @@ def verificar_permissao():
 def conectarbanco():
     try:
         conn = mysql.connector.connect(
-            host="maglev.proxy.rlwy.net",
-            port=10175,
-            user="root",
-            password="DrMCLnXdmCSDqBsJSiZzXmfaIxHvMkkL",
-            database="railway"
+            host="mysql_atoscapital",
+            port=3306,
+            user="atos",
+            password="atoscapital",
+            database="atoscapital"
         )
         return conn
     except mysql.connector.Error as e:
